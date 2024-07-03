76abbbb812d0acd31fbd1ac2a640502f626a06e4
marcelin-a <marcelinalley@yahoo.com>
2024-07-03 11:04:54 +0200

    first commit (movies and users)

--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -24,21 +24,59 @@ const movies = [
     duration: 180,
   },
 ];
-
+/*
 const getMovies = (req, res) => {
   res.json(movies);
 };
+*/
 
+const database = require("../../database");
+
+const getMovies = (req, res) => {
+  database
+    .query("select * from movies")
+    .then(([movies]) => {
+      res.json(movies); // use res.json instead of console.log
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+/*
 const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
   const movie = movies.find((movie) => movie.id === id);
+
+  console.log(movie.id)
 
   if (movie != null) {
     res.json(movie);
   } else {
     res.status(404).send("Not Found");
   }
+};
+*/
+
+const getMovieById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("select * from movies where id = ?", [id])
+
+    .then(([movies]) => {
+      if (movies[0] != null) {
+        res.json(movies[0]);
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 };
 
 module.exports = {
